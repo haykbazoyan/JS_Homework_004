80ad5d710578879c3fcf9e8e5108e0907969eef7
Hayk Bazoyan <bazoyan13@gmail.com>
2021-04-29 13:22:44 +0400

    Answer in first task from js.info

---
+++ arrayTask01.js
@@ -0,0 +1,10 @@
+// What is this code going to show?
+
+let phonesName = ["iPhone", "Samsung", "Oppa"];
+let inStockList = phonesName;
+
+inStockList.push("Honor");
+console.log(phonesName);
+console.log(inStockList);
+
+//phonesName & inStockList will be the same, because both arrays have a same reference
